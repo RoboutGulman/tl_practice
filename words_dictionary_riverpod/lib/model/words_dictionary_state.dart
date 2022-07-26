d89e9198dcbeff28de15431365d7699b6e88e441
Alex <AlexeyZ213@yandex.ru>
2022-07-26 17:21:04 +0300

    words_dictionry_riverpod:  bug with checkbox fixed

--- words_dictionary_riverpod/lib/model/words_dictionary_state.dart
+++ words_dictionary_riverpod/lib/model/words_dictionary_state.dart
@@ -30,12 +30,12 @@ final currentLanguagesProvider =
 
 @immutable
 class WordsDictionary {
-  const WordsDictionary(this.wordList, this.selectedWordIds);
+  const WordsDictionary(this.wordList, this.selectedWordIds, this.maxId);
 
   final List<Word> wordList;
   final Set<int> selectedWordIds;
+  final int maxId;
 
-  //передавать current languages аргументом
   void printSelectedWords(CurrentLanguages currentLanguages) {
     final result =
         wordList.where((word) => selectedWordIds.contains(word.id)).map((word) {
@@ -49,43 +49,50 @@ class WordsDictionary {
   }
 }
 
+int _maxIdInList(List<Word> wordList) {
+  var maxId = 0;
+  for (var element in wordList) {
+    if (element.id > maxId) {
+      maxId = element.id;
+    }
+  }
+  return maxId;
+}
+
 class WordsDictionaryNotifier extends StateNotifier<WordsDictionary> {
   WordsDictionaryNotifier(List<Word> wordList, Set<int> selectedWordIds)
-      : super(WordsDictionary(wordList, selectedWordIds));
-
-  void printSelectedWords(CurrentLanguages currentLanguages) {
-    state.printSelectedWords(currentLanguages);
-  }
+      : super(
+            WordsDictionary(wordList, selectedWordIds, _maxIdInList(wordList)));
 
   void addWord(Map<Language, String> translations) {
-    var newWordList = state.wordList;
-    final newWordtId = newWordList.length + 1;
+    var newWordList = state.wordList.toList();
+    final newWordtId = state.maxId + 1;
     final newWord = Word(
         id: newWordtId, translations: Map<Language, String>.from(translations));
     newWordList.add(newWord);
-    state = WordsDictionary(newWordList, state.selectedWordIds);
+    state = WordsDictionary(newWordList, state.selectedWordIds, newWordtId);
   }
 
   void removeSelectedWords() {
-    var newWordList = state.wordList;
+    var newWordList = state.wordList.toList();
     newWordList
         .removeWhere((element) => state.selectedWordIds.contains(element.id));
-    state = WordsDictionary(newWordList, {});
+    state = WordsDictionary(newWordList, {}, state.maxId);
   }
 
   void selectWord(int wordId) {
     if (!state.selectedWordIds.contains(wordId)) {
-      var newSelectedWordIds = state.selectedWordIds;
+      var newSelectedWordIds = state.selectedWordIds.toSet();
       newSelectedWordIds.add(wordId);
-      state = WordsDictionary(state.wordList, newSelectedWordIds);
+      state = WordsDictionary(state.wordList, newSelectedWordIds, state.maxId);
     }
   }
 
   void unselectWord(int wordId) {
     if (state.selectedWordIds.contains(wordId)) {
-      var newSelectedWordIds = state.selectedWordIds;
+      var newSelectedWordIds = state.selectedWordIds.toSet();
       newSelectedWordIds.remove(wordId);
-      state = WordsDictionary(state.wordList, newSelectedWordIds);
+      state = WordsDictionary(state.wordList, newSelectedWordIds, state.maxId);
     }
   }
 }
